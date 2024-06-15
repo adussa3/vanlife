1f4f6f164b154de95ca80ccd29fd3e43dfad2805
Anunoy Dussa <adussa3@gmail.com>
2024-06-14 22:54:56 -0500

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+/*
+  BrowserRouter
+  What BrowserRouter does under the hood is that it's a ContextProvider!
+  It provides context to all of its children components
+
+  So in order to give our app the power of routing in its functionality, 
+  we need to wrap everything inside of the App component inside of BrowserRouter
+
+  Route
+  A Route is part of your URL that specifies where you are on your website
+
+  For example, example.com/ is your home page and its route is "/" and
+  example.com/about is your about page and its route is "/about"
+*/
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <header>
+                <NavLink className="site-logo" to="/">
+                    #VanLife
+                </NavLink>
+                <nav>
+                    <NavLink to="/about">About</NavLink>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
