d36d97d9a60d8dd25a4be5884b16a0d67c0cda92
Anunoy Dussa <adussa3@gmail.com>
2024-06-26 10:44:42 -0500

    Added Footer

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Layout() {
     return (
-        <>
+        <div className="site-wrapper">
             <Header />
             {/*
                 The <Oulet> component is a way to create a "hole" in whatever our component is
                 where the matching route - that is a child of the Layout route - can render
             */}
-            <Outlet />
-        </>
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     );
 }
