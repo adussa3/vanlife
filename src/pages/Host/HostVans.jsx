d1a9337f75281e195256e2de3dbacf9833eb0ec5
Anunoy Dussa <adussa3@gmail.com>
2024-06-27 20:40:29 -0500

    Added a page that shows a list of all the host's vans

---
+++ src/pages/Host/HostVans.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function HostVans() {
+    const [hostVans, setHostVans] = useState([]);
+
+    useEffect(() => {
+        async function fetchHostVans() {
+            const response = await fetch("/api/host/vans");
+            const data = await response.json();
+            const vans = data.vans;
+            setHostVans(vans);
+        }
+        fetchHostVans();
+    }, []);
+
+    const hostVansEls = hostVans.map((van) => (
+        <Link to={`/host/vans/${van.id}`} key={van.id} className="host-van-link-wrapper">
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ));
+
+    return (
+        <section>
+            <h1 className="host-vans-title">Your listed vans</h1>
+            <div className="host-vans-list">{hostVans.length > 0 ? <section>{hostVansEls}</section> : <h2>Loading...</h2>}</div>
+        </section>
+    );
+}
